b7cfdebd3e7e24e0b2e30f1b17530f3afd22de2b
GabriellaMansur <gabriellamesquita314@gmail.com>
2024-03-08 17:53:15 -0300

    conceitos iniciais

---
+++ exercicios/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View  } from 'react-native'
+
+import Primeiro from'./components/Primeiro'
+
+
+export default () => (
+    <View>
+        <Primeiro></Primeiro>
+    </View>
+)
